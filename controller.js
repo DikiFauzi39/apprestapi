df0ebf53e49d21c803cad616df6a46340b24d62f
DikiFauzi39 <darsenal39@gmail.com>
2021-12-19 16:11:34 +0700

    buat fungsi get data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,15 @@ const conncection= require('./koneksi');
 exports.index = function(req, res) {
     response.ok("Success Running!!",res)
 };
+
+// Menampilkan Data
+
+exports.tampilsemuamahasiswa = function (req,res) {
+    conncection.query('Select * from mahasiswa', function(error, rows, fields){
+        if(error){
+            conncection.log(error);
+        }else{
+            response.ok(rows,res)
+        }
+    });
+}
