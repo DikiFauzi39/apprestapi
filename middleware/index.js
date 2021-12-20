3b76b2462acc7078f2fe082f27b4b11fffad1842
DikiFauzi39 <darsenal39@gmail.com>
2021-12-20 20:52:14 +0700

    login jwt

--- middleware/index.js
+++ middleware/index.js
@@ -4,5 +4,6 @@ const auth = require('./auth');
 const router = express.Router();
 
 router.post('/api/v1/register',auth.resgistrasi);
+router.post('/api/v1/login',auth.login);
 
 module.exports= router;
